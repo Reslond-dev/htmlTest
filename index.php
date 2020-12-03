991784641531742b01dc086dec2f2e5f9219a918
Reslond-dev <christophercmathieu@gmail.com>
2020-12-03 16:26:12 +0900

    第●章 第●部

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>tech boost</title>
+</head>
+  <body>
+    <h1>Hello world!!!</h1>
+    <h1>見出し1</h1>
+    <p>はじめてのwebサイトへようこそ</p>
+    <p>テキスト</p>
+    <a href="https://google.com">Google</a>
+    <img src="https://b-engineer.co.jp/wp-content/themes/b-engineer.co.jp/assets/img/common/logo_l.svg" alt="Branding Engineerのロゴ">
+    <ul>
+  <li>項目1</li>
+  <li>項目2</li>
+  <li>項目3</li>
+</ul>
+<ol>
+  <li>項目1</li>
+  <li>項目2</li>
+  <li>項目3</li>
+</ol>
+<header>
+  ここにいくつか要素が入ります
+</header>
+<footer>
+  ここにいくつか要素が入ります
+</footer>
+<section>
+  ここにいくつか要素が入ります
+</section>
+  </body>
+</html>
